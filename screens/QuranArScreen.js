475a748e0f330e9c34b87982c139173011ce8034
khaledyou87 <khaledyou87@gmail.com>
2022-08-20 13:32:51 +0300

    add quran list

--- screens/QuranArScreen.js
+++ screens/QuranArScreen.js
@@ -1,11 +1,171 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Screen from '../components/Screen'
+import { Entypo,AntDesign } from '@expo/vector-icons';
 
 const QuranArScreen = () => {
   return (
-    <View>
-      <Text>QuranArScreen</Text>
-    </View>
+    <Screen>
+      <View className=" items-center  p-7">
+        <View className="bg-purple px-12   rounded-2xl ">
+        <Text className="p-3 text-white">
+          afahras
+        </Text>
+        </View>
+      </View>
+
+
+ <View style={{borderWidth:1}}  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24  border-purple content-center pt-3 mb-5 ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>ال عمران</Text>
+
+   
+   </View>
+   
+   
+ </View>
+ <View style={{borderWidth:1}}  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24  border-purple content-center pt-3 mb-5 ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>الفاتحة</Text>
+
+   
+   </View>
+   
+   
+ </View>
+ <View style={{borderWidth:1}}  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24  border-purple content-center pt-3 mb-5 ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>ق</Text>
+
+   
+   </View>
+   
+   
+ </View>
+ <View style={{borderWidth:1}}  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24  border-purple content-center pt-3 mb-5 ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>الذاريات</Text>
+
+   
+   </View>
+   
+   
+ </View>
+ <View style={{borderWidth:1}}  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24  border-purple content-center pt-3 mb-5 ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>الفاتحة</Text>
+
+   
+   </View>
+   
+   
+ </View>
+
+ 
+ 
+ 
+ 
+ <View  className=" flex-row pb-3 space-x-2 mx-4 items-center bg-white rounded-2xl h-24 border border-purple content-center pt-3 mb ">
+   
+   <View className='flex-1 m-4 mx-7  '>
+   
+     
+     <Text className='font-bold text-xl text-black '>
+     Alfatiha
+      </Text>
+     <Text className=' text-sm text-purple '>
+     madnia
+      </Text>
+      
+   </View>
+   
+   
+   <View>
+
+   <Text  className='font-bold  text-2xl text-black m-4 mx-7'>الفاتحة</Text>
+
+
+   </View>
+   
+   
+ </View>
+
+    </Screen>
   )
 }
 
